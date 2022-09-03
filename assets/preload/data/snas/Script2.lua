37fde87ffab245df57c0e8c4bb58428f62a27f45
Rednosus <103311757+Rednosus@users.noreply.github.com>
2022-09-03 09:33:45 +0700

    Add files via upload

---
+++ assets/preload/data/snas/Script2.lua
@@ -0,0 +1,30 @@
+function onStepHit()
+
+	if curStep == 919 then
+		setProperty('defaultCamZoom', 1.5)
+end
+
+	if curStep == 991 then
+		setProperty('defaultCamZoom', 0.9)
+end
+
+	if curStep == 1013 then
+		cameraFlash('game', 'FFFFFF', 0.1, true)
+end
+
+	if curStep == 1014 then
+		cameraFlash('game', 'FFFFFF', 0.1, true)
+end
+
+	if curStep == 1015 then
+		cameraFlash('game', 'FFFFFF', 0.1, true)
+end
+
+	if curStep == 1023 then
+		cameraFlash('game', 'FFFFFF', 0.1, true)
+end
+
+	if curStep == 1424 then
+		cameraFlash('game', 'FFFFFF', 0.1, true)
+	end
+end
